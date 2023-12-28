ba8fdb877160c948bd43f4900d442e66511d8b01
M-Fahad-Jahangir <m.fahadme@gmail.com>
2023-12-28 17:38:02 +0500

    custom carousel done

---
+++ Js/carousel.js
@@ -0,0 +1,6 @@
+let toggleClass = () => {
+    document.getElementById("page1").classList.toggle("slide1");
+    document.getElementById("page1").classList.toggle("slide2");
+    document.getElementById("page2").classList.toggle("slide1");
+    document.getElementById("page2").classList.toggle("slide2");
+};
